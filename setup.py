1c81a9573d6c5eda3e63bb541a180846d8424fba
Kvar_ispw17 <kvar_ispw17@localhost.localdomain>
2017-09-16 13:31:06 +0800

    Update PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pkuchive',
+  packages = ['pkuchive'], # this must be the same as the name above
+  version = '1.0.1',
+  description = 'A tool for OIer to tag the archive folder provided by PKU openjudge (POJ) archive service.',
+  author = 'Kvar_ispw17',
+  author_email = 'enkerewpo@gmail.com',
+  url = 'https://github.com/enkerewpo/pkuchive', # use the URL to the github repo
+  download_url = 'https://github.com/enkerewpo/pkuchive/releases/download/1.0.1/pkuchive_release_1.0.1_20170706.tar.gz', # I'll explain this in a second
+  keywords = ['acm-icpc','noip','noi','openjudge '], # arbitrary keywords
+  classifiers = [],
+)
